eda4031778c243e2e05d0e078264d61a2a8cca48
maho-ya <github@maho-ya.com>
2020-05-18 09:00:48 +0900

    fist commit

---
+++ app/src/main/java/com/maho_ya/tell_me_your_dpi/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.maho_ya.tell_me_your_dpi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
