b1e06b4a2b05afa80fe468aad9ddafd6ed96b613
maho-ya <github@maho-ya.com>
2020-06-05 09:32:07 +0900

    Change class name from MyApplication to MainApplication.

--- app/src/main/java/com/maho_ya/tell_me_your_dpi/MainApplication.kt
+++ app/src/main/java/com/maho_ya/tell_me_your_dpi/MainApplication.kt
@@ -2,6 +2,6 @@ package com.maho_ya.tell_me_your_dpi
 
 import androidx.multidex.MultiDexApplication
 
-class MyApplication: MultiDexApplication() {
+class MainApplication: MultiDexApplication() {
 
 }
