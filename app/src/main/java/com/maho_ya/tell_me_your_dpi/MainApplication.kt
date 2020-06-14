9820482688f9d90e43973e63f1135c8291f60514
maho-ya <github@maho-ya.com>
2020-06-14 09:59:25 +0900

    Add a setting to use Timber log

--- app/src/main/java/com/maho_ya/tell_me_your_dpi/MainApplication.kt
+++ app/src/main/java/com/maho_ya/tell_me_your_dpi/MainApplication.kt
@@ -1,7 +1,20 @@
 package com.maho_ya.tell_me_your_dpi
 
 import androidx.multidex.MultiDexApplication
+import timber.log.Timber
+
+import timber.log.Timber.DebugTree
+
+
+
 
 class MainApplication: MultiDexApplication() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
 }
