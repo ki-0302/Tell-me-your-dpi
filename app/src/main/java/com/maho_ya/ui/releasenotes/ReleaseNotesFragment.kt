f6aca85b96aa8dd734e521197e6391cac25b547e
maho-ya <github@maho-ya.com>
2020-06-15 09:23:30 +0900

    Add release notes fragment

---
+++ app/src/main/java/com/maho_ya/ui/releasenotes/ReleaseNotesFragment.kt
@@ -0,0 +1,13 @@
+package com.maho_ya.ui.releasenotes
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.maho_ya.tell_me_your_dpi.R
+
+class ReleaseNotesFragment: Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
