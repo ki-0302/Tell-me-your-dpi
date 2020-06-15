9830ffe218bb9a3ea37e09f053aa304d67329218
maho-ya <github@maho-ya.com>
2020-06-15 09:10:34 +0900

    Remove unnecessary files

--- app/src/main/java/com/maho_ya/tell_me_your_dpi/MyNavHostFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.maho_ya.tell_me_your_dpi
-
-import androidx.navigation.fragment.NavHostFragment
-
-class MyNavHostFragment: NavHostFragment() {
-}
--- app/src/main/java/com/maho_ya/tell_me_your_dpi/PrivacyFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.maho_ya.tell_me_your_dpi
-
-import androidx.annotation.Keep
-import androidx.fragment.app.Fragment
-
-class PrivacyFragment: Fragment(R.layout.fragment_privacy) {
-}
