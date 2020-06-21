ca9d0f5628e24c8221263dbdf3689b29de78ee8e
maho-ya <github@maho-ya.com>
2020-06-22 08:08:11 +0900

    Add Result class for to get UseCase results.

--- app/src/main/java/com/maho_ya/result/Result.kt
+++ app/src/main/java/com/maho_ya/result/Result.kt
@@ -2,6 +2,11 @@ package com.maho_ya.result
 
 sealed class Result<out R> {
 
+    object Loading : Result<Nothing>()
     data class Success<out T>(val data: T) : Result<T>()
     data class Error(val exception: Exception) : Result<Nothing>()
 }
+
+// Kotlin Custom Property. Return data on success.
+val <T> Result<T>.data: T?
+    get() = (this as? Result.Success)?.data
