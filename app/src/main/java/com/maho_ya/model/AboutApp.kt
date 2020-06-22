a871e84e08c8100deb5a68bfb05c262871308092
maho-ya <github@maho-ya.com>
2020-06-22 09:13:09 +0900

    Remove unnecessary files

--- app/src/main/java/com/maho_ya/model/AboutApp.kt
+++
@@ -1,6 +0,0 @@
-package com.maho_ya.model
-
-class AboutApp(
-    val key: AboutAppKey,
-    val menuContent: String?
-)
--- app/src/main/java/com/maho_ya/model/AboutAppKey.kt
+++
@@ -1,7 +0,0 @@
-package com.maho_ya.model
-
-enum class AboutAppKey(val key: Int) {
-    VERSION(1),
-    PRIVACY(2),
-    LICENSES(3)
-}
