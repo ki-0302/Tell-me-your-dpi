97fb46eef63a172c737ed076de6a66968dbac073
maho-ya <github@maho-ya.com>
2020-07-03 08:15:35 +0900

    Change to use @Component.Builder and @BindsInstance in DI

--- app/src/main/java/com/maho_ya/di/ApplicationComponent.kt
+++ app/src/main/java/com/maho_ya/di/ApplicationComponent.kt
@@ -1,22 +1,32 @@
 package com.maho_ya.di
 
+import android.content.Context
+import com.maho_ya.MainApplication
 import com.maho_ya.ui.MainActivity
 import com.maho_ya.ui.aboutapp.AboutAppFragment
 import com.maho_ya.ui.home.HomeFragment
 import com.maho_ya.ui.releasenotes.ReleaseNotesFragment
+import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(
     modules = [
-        AppModule::class,
         DeviceModule::class,
         ReleaseNotesModule::class,
         NetworkModule::class
     ]
 )
 interface ApplicationComponent {
+
+    @Component.Builder
+    interface Builder {
+        fun build(): ApplicationComponent
+        // @BindsInstance have been added since Dagger Ver.2.9. Arguments inject to component.
+        @BindsInstance
+        fun application(applicationContext: Context): Builder
+    }
 
     fun inject(mainActivity: MainActivity)
     fun inject(homeFragment: HomeFragment)
