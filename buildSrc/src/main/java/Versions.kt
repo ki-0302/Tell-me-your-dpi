17b9e419b2214c11ae3d7d5937b00cd982409fc2
maho-ya <github@maho-ya.com>
2020-07-10 08:05:55 +0900

    Migration from gradle to Kotlin DSL

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,18 @@
+object Versions {
+    val versionName = "1.1"
+    val versionCode = 3
+
+    const val COMPILE_SDK = 29
+    const val TARGET_SDK = 29
+    const val MIN_SDK = 21
+
+    const val ANDROID_GRADLE_PLUGIN = "4.0.0"
+    const val KOTLIN = "1.3.72"
+    const val NAVIGATION = "2.3.0"
+    const val KTLINT = "9.2.1"
+    const val HILT = "2.28-alpha"
+    const val GOOGLE_SERVICES = "4.3.3"
+    const val FIREBASE_CRASHLYTICS = "2.2.0"
+    const val FIREBASE_PERF = "1.3.1"
+    const val OSS_LICENSES = "0.10.2"
+}
