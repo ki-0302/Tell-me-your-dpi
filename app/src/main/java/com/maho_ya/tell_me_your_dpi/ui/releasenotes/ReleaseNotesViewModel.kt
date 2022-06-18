d4c6dab1ccd9655bb994f8585524b8e69cb831f9
maho-ya <github@maho-ya.com>
2022-06-19 00:08:20 +0900

    feat: Migration release note to jetpack compose

--- app/src/main/java/com/maho_ya/tell_me_your_dpi/ui/releasenotes/ReleaseNotesViewModel.kt
+++ app/src/main/java/com/maho_ya/tell_me_your_dpi/ui/releasenotes/ReleaseNotesViewModel.kt
@@ -1,27 +1,17 @@
 package com.maho_ya.tell_me_your_dpi.ui.releasenotes
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
-import androidx.lifecycle.map
-import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
+import com.maho_ya.tell_me_your_dpi.R
 import com.maho_ya.tell_me_your_dpi.domain.releasenotes.ReleaseNotesUseCase
 import com.maho_ya.tell_me_your_dpi.model.ReleaseNote
 import com.maho_ya.tell_me_your_dpi.result.Result
-import com.maho_ya.tell_me_your_dpi.result.data
 import com.maho_ya.tell_me_your_dpi.util.WhileUiSubscribed
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.switchMap
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import org.jetbrains.annotations.Async
 import javax.inject.Inject
 
 data class ReleaseNoteUiState(
@@ -36,31 +26,13 @@ class ReleaseNotesViewModel @Inject constructor(
     private val releaseNotesUseCase: ReleaseNotesUseCase
 ) : ViewModel(), ReleaseNotesEventListener {
 
-    private val _isLoading = MutableStateFlow(false)
-    private val _releaseNotesState = MutableStateFlow<Result<List<ReleaseNote>>>(Result.Loading)
+    private val _releaseNotesUiState = MutableStateFlow(ReleaseNoteUiState(isLoading = true))
 
-    val uiState: StateFlow<ReleaseNoteUiState> = combine(
-        _isLoading, _releaseNotesState
-    ) { isLoading, releaseNotesState ->
-        when (releaseNotesState) {
-            is Result.Loading -> ReleaseNoteUiState(isLoading = true)
-            is Result.Success -> {
-                ReleaseNoteUiState(
-                    items = releaseNotesState.data,
-                    isLoading = isLoading,
-                    userMessage = null
-                )
-            }
-            is Result.Error -> {
-                // TODO エラーメッセージを渡す
-                ReleaseNoteUiState(isLoading = true)
-            }
-        }
-        // FlowをStateFlowに変換。startedはViewで使用する場合は WhileSubscribed を使用する
-    }.stateIn(
+    // StateFlowに変換。startedはViewで使用する場合は WhileSubscribed を使用する
+    val uiState = _releaseNotesUiState.stateIn(
         scope = viewModelScope,
         started = WhileUiSubscribed,
-        initialValue = ReleaseNoteUiState(isLoading = true)
+        initialValue = _releaseNotesUiState.value
     )
 
     init {
@@ -72,12 +44,32 @@ class ReleaseNotesViewModel @Inject constructor(
     }
 
     private fun loadReleaseNotes() {
-        _isLoading.value = true
+        _releaseNotesUiState.update { it.copy(isLoading = true) }
+
         viewModelScope.launch {
             // collect method of coroutine executes a suspend fun. It then collects a flow.
-            releaseNotesUseCase().collect {
-                _releaseNotesState.value = it
-                _isLoading.value = false
+            releaseNotesUseCase().collect { result ->
+                when (result) {
+                    is Result.Loading -> _releaseNotesUiState.update { it.copy(isLoading = true) }
+                    is Result.Success -> {
+                        _releaseNotesUiState.update {
+                            it.copy(
+                                isLoading = false,
+                                items = result.data,
+                                userMessage = null
+                            )
+                        }
+                    }
+                    is Result.Error -> {
+                        _releaseNotesUiState.update {
+                            it.copy(
+                                isLoading = false,
+                                items = emptyList(),
+                                userMessage = R.string.network_error_title
+                            )
+                        }
+                    }
+                }
             }
         }
     }
